1785cf3f785f722d8bb33c63fa7bd3155923f048
Mickaël Lafages <mickx971.l@gmail.com>
2017-05-17 00:49:34 +0200

    Nao global behaviour (in progress)

--- nao/Nao.py
+++ nao/Nao.py
@@ -5,90 +5,124 @@ Created on 28 April 2017
 @author: Mickaël Lafages
 @description: Nao respond to orders
 '''
-import naoutil.naoenv as naoenv
-import naoutil.memory as memory
+#import naoutil.naoenv as naoenv
+#import naoutil.memory as memory
 from community.core import Agent
-from naoutil.broker import Broker
-from fluentnao.nao import Nao
+#from naoutil.broker import Broker
+#from fluentnao.nao import Nao
 from community.statechart import EventFSMBehaviour
 from community.behaviours.executor import TaskExecutor
 from community.behaviours.system import SystemCore
+from nao.behaviours.communication import Communicator
 from time import sleep
-import qi
+#import qi
 from threading import Lock
 
 
 class NaoAgent(Agent):
 
+    def __init__(self, name, host, secret, selfIP):
+        #self.naoqiInit(selfIP)
+        behaviours = [SystemCore(), TaskExecutor(), self.createEventFSMBehaviour(), Communicator()]
+        Agent.__init__(self, name, host, secret, behaviours, [])
+
     EVENT_ORDER_LISTEN = "EVENT_ORDER_LISTEN"
     EVENT_ORDER_SIT_DOWN = "EVENT_ORDER_SIT_DOWN"
     EVENT_ORDER_STAND_UP = "EVENT_ORDER_STAND_UP"
     EVENT_ORDER_SAY_HI = "EVENT_ORDER_SAY_HI"
-    SALLE_1_RAPHAEL = "SALLE_1_RAPHAEL"
-    SALLE_2_RAPHAEL = "SALLE_2_RAPHAEL"
-    SALLE_3_RAPHAEL = "SALLE_3_RAPHAEL"
+    EVENT_SALLE_1_RAPHAEL = "EVENT_SALLE_1_RAPHAEL"
+    EVENT_SALLE_2_RAPHAEL = "EVENT_SALLE_2_RAPHAEL"
+    EVENT_SALLE_3_RAPHAEL = "EVENT_SALLE_3_RAPHAEL"
+    EVENT_SALLE_1_SAMIRA = "EVENT_SALLE_1_SAMIRA"
+    EVENT_SALLE_2_SAMIRA = "EVENT_SALLE_2_SAMIRA"
+    EVENT_SALLE_3_SAMIRA = "EVENT_SALLE_3_SAMIRA"
+    EVENT_RASSEMBLEMENT = "EVENT_RASSEMBLEMENT"
+    EVENT_POUSSEZ = "EVENT_POUSSEZ"
+    EVENT_TURTLE_SAY = "EVENT_TURTLE_SAY"
+    EVENT_MOVE = "EVENT_MOVE"
 
-    def listenCall(myAgent):
+    def listenCall(myAgent, inputs, eventInputs):
+        myAgent.log(inputs, "input")
+        myAgent.log(eventInputs, "eventInputs")
         myAgent.log("listen Call", "LISTEN_CALL")
 
-    def listenOrder(myAgent):
+    def listenOrder(myAgent, inputs, eventInputs):
+        myAgent.log(inputs, "input")
+        myAgent.log(eventInputs, "eventInputs")
         myAgent.log("listenOrder", "LISTEN_ORDER")
         myAgent.say("J'écoute")
 
-    def sitDown(myAgent):
+    def sitDown(myAgent, inputs, eventInputs):
+        myAgent.log(inputs, "input")
+        myAgent.log(eventInputs, "eventInputs")
         myAgent.log("sitDown", "SIT_DOWN")
-        myAgent.nao.sit()
+        #myAgent.nao.sit()
         return 0
 
-    def standUp(myAgent):
+    def standUp(myAgent, inputs, eventInputs):
+        myAgent.log(inputs, "input")
+        myAgent.log(eventInputs, "eventInputs")
         myAgent.log("standUp", "STAND_UP")
-        myAgent.nao.stand()
+        #myAgent.nao.stand()
         return 0
 
-    def sayHi(myAgent):
+    def sayHi(myAgent, inputs, eventInputs):
+        myAgent.log(inputs, "input")
+        myAgent.log(eventInputs, "eventInputs")
         myAgent.log("sayHi", "SAY_HI")
-        myAgent.sayAnimated("^start(animations/Stand/Gestures/Hey_1) Oui, je veux faire un coucou à tout le monde! ^wait(animations/Stand/Gestures/Hey_1)")
+        myAgent.sayAnimated("^start(animations/Stand/Gestures/Hey_1) Coucou ! ^wait(animations/Stand/Gestures/Hey_1)")
         return 0
 
     def createTurtleOrder(self, turtleName, orderName, data):
-        def action(myAgent):
+        def action(myAgent, inputs, eventInputs):
+            myAgent.log(inputs, "input")
+            myAgent.log(eventInputs, "eventInputs")
             myAgent.say(orderName)
         return action
 
-    def __init__(self, name, host, secret, selfIP):
+    def sendGetherOrder(myAgent, inputs, eventInputs):
+        myAgent.log(inputs, "input")
+        myAgent.log(eventInputs, "eventInputs")
+        myAgent.log("gether", "RASSEMBLEMENT")
 
+    def sendPushOrder(myAgent, inputs, eventInputs):
+        myAgent.log(inputs, "input")
+        myAgent.log(eventInputs, "eventInputs")
+        myAgent.log("push", "POUSSEZ")
+
+    def onHumanMoveEvent(myAgent, inputs, eventInputs):
+        myAgent.log(inputs, "input")
+        myAgent.log(eventInputs, "eventInputs")
+        myAgent.log("move", "onHumanMoveEvent")
+
+    def onTurtleSayEvent(myAgent, inputs, eventInputs):
+        myAgent.log(inputs, "input")
+        myAgent.log(eventInputs, "eventInputs")
+        myAgent.log("turtleSay", "onTurtleSayEvent")
+
+    def naoqiInit(self, selfIP):
         speechEvents = dict()
-        speechEvents["nao écoute moi"] = NaoAgent.EVENT_ORDER_LISTEN
-        speechEvents["Veux-tu dire quelque chose ?"] = NaoAgent.EVENT_ORDER_SAY_HI
-        speechEvents["Assieds-toi"] = NaoAgent.EVENT_ORDER_SIT_DOWN
-        speechEvents["Mets-toi debout"] = NaoAgent.EVENT_ORDER_STAND_UP
-        speechEvents["Raphael"] = NaoAgent.SALLE_1_RAPHAEL
-        speechEvents["Donatello"] = NaoAgent.SALLE_1_RAPHAEL
-        speechEvents["Dis à Raphael d'aller à la salle 2"] = NaoAgent.SALLE_2_RAPHAEL
-        speechEvents["Dis à Raphael d'aller à la salle 3"] = NaoAgent.SALLE_3_RAPHAEL
-        speechEvents["Ordonne d'aller dans la première salle"] = NaoAgent.SALLE_3_RAPHAEL
-        speechEvents["Ordonne d'aller dans la deuxième salle"] = NaoAgent.SALLE_3_RAPHAEL
-        speechEvents["Ordonne d'aller dans la troisième salle"] = NaoAgent.SALLE_3_RAPHAEL
-
-
-        recognitionRate = dict()
-        recognitionRate["nao écoute moi"] = .37
-        recognitionRate["Assieds-toi"] = .40
-        recognitionRate["Mets-toi debout"] = .35
-        recognitionRate["Veux-tu dire quelque chose ?"] = .20
-        recognitionRate["Dis à Raphael d'aller à la salle 1"] = .35
-        recognitionRate["Dis à Raphael d'aller à la salle 2"] = .35
-        recognitionRate["Dis à Raphael d'aller à la salle 3"] = .35
+        speechEvents["nao écoute moi"] = [NaoAgent.EVENT_ORDER_LISTEN, .37]
+        speechEvents["Bonjour nao"] = [NaoAgent.EVENT_ORDER_SAY_HI, .40]
+        speechEvents["Assieds-toi"] = [NaoAgent.EVENT_ORDER_SIT_DOWN, .40]
+        speechEvents["Mets-toi debout"] = [NaoAgent.EVENT_ORDER_STAND_UP, .30]
+        speechEvents["Raphael un"] = [NaoAgent.EVENT_SALLE_1_RAPHAEL, .27]
+        speechEvents["Raphael deuxième"] = [NaoAgent.EVENT_SALLE_2_RAPHAEL, .30]
+        speechEvents["Raphael troisième"] = [NaoAgent.EVENT_SALLE_3_RAPHAEL, .30]
+        speechEvents["Samira un"] = [NaoAgent.EVENT_SALLE_1_SAMIRA, .27]
+        speechEvents["Samira deuxième"] = [NaoAgent.EVENT_SALLE_2_SAMIRA, .30]
+        speechEvents["Samira troisième"] = [NaoAgent.EVENT_SALLE_3_SAMIRA, .30]
+        speechEvents["Rassemblement"] = [NaoAgent.EVENT_RASSEMBLEMENT, .35]
+        speechEvents["Poussez la boite"] = [NaoAgent.EVENT_POUSSEZ, .30]
 
         # callbacks
         def recognitionCallback(dataName, value, message):
 
             d = dict(zip(value[0::2], value[1::2]))
-            self.log(value[0] + " " + str(value[1]))
 
             self.log("acquire with listening: " + str(self.isListening), "recognitionCallback")
             self.lock.acquire()
-            if not self.isListening :
+            if not self.isListening:
                 self.isListening = True
                 self.lock.release()
                 self.log("release 1", "recognitionCallback")
@@ -97,8 +131,12 @@ class NaoAgent(Agent):
             self.log("release 2", "recognitionCallback")
 
             for word in d:
-                if word in recognitionRate and d[word] > recognitionRate[word]:
-                    self.raiseEvent(speechEvents[word])
+                self.log(word + " " + str(d[word]))
+                self.log("qdqdf " + word)
+                if d[word] > speechEvents[word][1]:
+                    self.log("1")
+                    self.log(speechEvents[word][0])
+                    self.raiseEvent(speechEvents[word][0])
                     return
 
             self.say("Je n'ai pas bien compris")
@@ -125,13 +163,19 @@ class NaoAgent(Agent):
             self.lock.release()
             self.log("release 1", "animatedSpeechCallback")
 
-            sleep(4)
+            sleep(5)
             self.log("acquire 2", "animatedSpeechCallback")
             self.lock.acquire()
             self.isListening = True
             self.lock.release()
             self.log("release 2", "animatedSpeechCallback")
 
+        def touchChangeCallback(eventName, touchInfo, subscriberIdentifier):
+            for i in touchInfo:
+                self.log(i)
+
+        def rigthHandCallback(eventName, val, subscriberIdentifier):
+            self.log(val)
 
         self.ip = selfIP
         self.broker = Broker('bootstrapBroker', naoIp=self.ip, naoPort=9559)
@@ -148,17 +192,19 @@ class NaoAgent(Agent):
         vocabulary.extend(speechEvents.keys())
 
         try:
-            self.nao.env.speechRecognition.setVocabulary(vocabulary, True)
+            self.nao.env.speechRecognition.pause(False)
+            self.nao.env.speechRecognition.setVocabulary(vocabulary, False)
         except RuntimeError as e:
+            self.nao.env.speechRecognition.pause(False)
             self.log(e.message, "RuntimeError")
 
         self.memory.subscribeToEvent('WordRecognized', recognitionCallback)
         self.memory.subscribeToEvent('ALTextToSpeech/Status', speechCallback)
         self.memory.subscribeToEvent('ALAnimatedSpeech/EndOfAnimatedSpeech', animatedSpeechCallback)
-
-        behaviours = [SystemCore(), TaskExecutor(), self.createEventFSMBehaviour()]
-
-        Agent.__init__(self, name, host, secret, behaviours, [])
+        # self.memory.subscribeToEvent('TouchChanged', touchChangeCallback)
+        # self.memory.subscribeToEvent('HandRightBackTouched', rigthHandCallback)
+        # self.memory.subscribeToEvent('HandRightLeftTouched', rigthHandCallback)
+        # self.memory.subscribeToEvent('HandRightRightTouched', rigthHandCallback)
 
     def createEventFSMBehaviour(self):
 
@@ -170,27 +216,62 @@ class NaoAgent(Agent):
         RAPHAEL_SALLE_1 = "RAPHAEL_SALLE_1"
         RAPHAEL_SALLE_2 = "RAPHAEL_SALLE_2"
         RAPHAEL_SALLE_3 = "RAPHAEL_SALLE_3"
+        SAMIRA_SALLE_1 = "SAMIRA_SALLE_1"
+        SAMIRA_SALLE_2 = "SAMIRA_SALLE_2"
+        SAMIRA_SALLE_3 = "SAMIRA_SALLE_3"
+        RASSEMBLEMENT = "RASSEMBLEMENT"
+        POUSSEZ = "POUSSEZ"
+        ON_MOVE_EVENT = "ON_MOVE_EVENT"
+        ON_TURTLE_SAY_EVENT = "ON_TURTLE_SAY_EVENT"
+        INIT = "INIT"
+
+        def init(myAgent, inputs, eventInputs):
+            self.log("init", "START_STATE")
 
         evfsm = EventFSMBehaviour("Liveness")
 
+        evfsm.createState(INIT, init)
         evfsm.createState(LISTEN_CALL, NaoAgent.listenCall)
         evfsm.createState(LISTEN_ORDER, NaoAgent.listenOrder)
         evfsm.createState(SIT_DOWN, NaoAgent.sitDown)
         evfsm.createState(STAND_UP, NaoAgent.standUp)
         evfsm.createState(SAY_HI, NaoAgent.sayHi)
         evfsm.createState(RAPHAEL_SALLE_1, self.createTurtleOrder("Raphael", "goTo", "salle1"))
-        evfsm.createState(RAPHAEL_SALLE_2, self.createTurtleOrder("Raphael", "goTo", "salle1"))
-        evfsm.createState(RAPHAEL_SALLE_3, self.createTurtleOrder("Raphael", "goTo", "salle1"))
+        evfsm.createState(RAPHAEL_SALLE_2, self.createTurtleOrder("Raphael", "goTo", "salle2"))
+        evfsm.createState(RAPHAEL_SALLE_3, self.createTurtleOrder("Raphael", "goTo", "salle3"))
+        evfsm.createState(SAMIRA_SALLE_1, self.createTurtleOrder("Samira", "goTo", "salle1"))
+        evfsm.createState(SAMIRA_SALLE_2, self.createTurtleOrder("Samira", "goTo", "salle2"))
+        evfsm.createState(SAMIRA_SALLE_3, self.createTurtleOrder("Samira", "goTo", "salle3"))
+        evfsm.createState(RASSEMBLEMENT, NaoAgent.sendGetherOrder)
+        evfsm.createState(POUSSEZ, NaoAgent.sendPushOrder)
+        evfsm.createState(ON_MOVE_EVENT, NaoAgent.onHumanMoveEvent)
+        evfsm.createState(ON_TURTLE_SAY_EVENT, NaoAgent.onTurtleSayEvent)
 
-        evfsm.createWaitingTransition(LISTEN_CALL, LISTEN_ORDER, NaoAgent.EVENT_ORDER_LISTEN)
+        evfsm.createWaitingTransition(INIT, ON_MOVE_EVENT, NaoAgent.EVENT_MOVE)
+        evfsm.createWaitingTransition(INIT, ON_TURTLE_SAY_EVENT, NaoAgent.EVENT_TURTLE_SAY)
+        evfsm.createWaitingTransition(INIT, LISTEN_CALL)
+
+        evfsm.createWaitingTransition(ON_MOVE_EVENT, ON_MOVE_EVENT, NaoAgent.EVENT_MOVE)
+        evfsm.createWaitingTransition(ON_TURTLE_SAY_EVENT, ON_TURTLE_SAY_EVENT, NaoAgent.EVENT_TURTLE_SAY)
+
+        evfsm.createMultiChoiceWaitingTransition()\
+            .fromState(LISTEN_CALL)\
+            .ifCondition(NaoAgent.EVENT_ORDER_LISTEN).goTo(LISTEN_ORDER)\
+            .elifCondition(NaoAgent.EVENT_ORDER_SAY_HI).goTo(SAY_HI) \
+            .create()
+
         evfsm.createMultiChoiceWaitingTransition() \
             .fromState(LISTEN_ORDER) \
             .ifCondition(NaoAgent.EVENT_ORDER_SIT_DOWN).goTo(SIT_DOWN) \
             .elifCondition(NaoAgent.EVENT_ORDER_STAND_UP).goTo(STAND_UP) \
-            .elifCondition(NaoAgent.EVENT_ORDER_SAY_HI).goTo(SAY_HI) \
-            .elifCondition(NaoAgent.SALLE_1_RAPHAEL).goTo(RAPHAEL_SALLE_1) \
-            .elifCondition(NaoAgent.SALLE_2_RAPHAEL).goTo(RAPHAEL_SALLE_2) \
-            .elifCondition(NaoAgent.SALLE_2_RAPHAEL).goTo(RAPHAEL_SALLE_3) \
+            .elifCondition(NaoAgent.EVENT_SALLE_1_RAPHAEL).goTo(RAPHAEL_SALLE_1) \
+            .elifCondition(NaoAgent.EVENT_SALLE_2_RAPHAEL).goTo(RAPHAEL_SALLE_2) \
+            .elifCondition(NaoAgent.EVENT_SALLE_2_RAPHAEL).goTo(RAPHAEL_SALLE_3) \
+            .elifCondition(NaoAgent.EVENT_SALLE_1_SAMIRA).goTo(SAMIRA_SALLE_1) \
+            .elifCondition(NaoAgent.EVENT_SALLE_2_SAMIRA).goTo(SAMIRA_SALLE_2) \
+            .elifCondition(NaoAgent.EVENT_SALLE_2_SAMIRA).goTo(SAMIRA_SALLE_3) \
+            .elifCondition(NaoAgent.EVENT_RASSEMBLEMENT).goTo(RASSEMBLEMENT) \
+            .elifCondition(NaoAgent.EVENT_POUSSEZ).goTo(POUSSEZ) \
             .create()
 
         evfsm.createSimpleTransition(SIT_DOWN, LISTEN_CALL, 0)
@@ -199,27 +280,37 @@ class NaoAgent(Agent):
         evfsm.createSimpleTransition(RAPHAEL_SALLE_1, LISTEN_CALL, 0)
         evfsm.createSimpleTransition(RAPHAEL_SALLE_2, LISTEN_CALL, 0)
         evfsm.createSimpleTransition(RAPHAEL_SALLE_3, LISTEN_CALL, 0)
+        evfsm.createSimpleTransition(SAMIRA_SALLE_1, LISTEN_CALL, 0)
+        evfsm.createSimpleTransition(SAMIRA_SALLE_2, LISTEN_CALL, 0)
+        evfsm.createSimpleTransition(SAMIRA_SALLE_3, LISTEN_CALL, 0)
+        evfsm.createSimpleTransition(RASSEMBLEMENT, LISTEN_CALL, 0)
+        evfsm.createSimpleTransition(POUSSEZ, LISTEN_CALL, 0)
 
-        evfsm.setStartingPoint(LISTEN_CALL)
+        evfsm.setStartingPoint(INIT)
 
         return evfsm
 
     def log(self, msg, module=""):
-        qi.logInfo(module, msg)
+        #qi.logInfo(module, msg)
+        print module, ":", msg
 
     def say(self, text):
-        self.isAnimatedSay = False
+        #self.isAnimatedSay = False
         self.log(text)
-        self.nao.say(text)
+        #self.nao.say(text)
 
     def sayAnimated(self, text):
-        self.isAnimatedSay = True
+        #self.isAnimatedSay = True
         self.log(text)
-        self.nao.animate_say(text)
+        #self.nao.animate_say(text)
 
     def takeDown(self):
         print "TakeDown"
-        self.memory.unsubscribeToEvent('WordRecognized')
-        self.memory.unsubscribeToEvent('ALTextToSpeech/Status')
-        self.memory.unsubscribeToEvent('ALAnimatedSpeech/EndOfAnimatedSpeech')
-        self.broker.shutdown()
+        #self.memory.unsubscribeToEvent('WordRecognized')
+        #self.memory.unsubscribeToEvent('ALTextToSpeech/Status')
+        #self.memory.unsubscribeToEvent('ALAnimatedSpeech/EndOfAnimatedSpeech')
+        # self.memory.unsubscribeToEvent('TouchChanged')
+        # self.memory.unsubscribeToEvent('HandRightBackTouched')
+        # self.memory.unsubscribeToEvent('HandRightLeftTouched')
+        # self.memory.unsubscribeToEvent('HandRightRightTouched')
+        #self.broker.shutdown()
